fed805314d3f93462eed5e6a5a4746617eae7de4
MoviePhoria <mpfilesbot@gmail.com>
2024-07-15 18:32:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -47,7 +47,8 @@ async def start(client, message):
                     InlineKeyboardButton('⚡ ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ⚡', callback_data='help')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_caption(
+        await message.reply_photo(
+            photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
@@ -86,7 +87,8 @@ async def start(client, message):
                     InlineKeyboardButton('⚡ ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ⚡', callback_data='help')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_caption(
+        await message.reply_photo(
+            photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
