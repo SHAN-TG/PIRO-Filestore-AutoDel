6602dd1d3906afd37f4162c592908bba9611a6b9
MoviePhoria <mpfilesbot@gmail.com>
2024-07-15 20:11:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,8 +18,8 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
-@Client.on_message(filters.command("start") & filters.incoming)
-async def startmp(client, message):
+@Client.on_message(filters.command("startmp") & filters.incoming)
+async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
